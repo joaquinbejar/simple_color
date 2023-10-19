2234a02a6947e98fb71022a66430667dfe628130
Joaquin Bejar <jbejar@swissblock.net>
2023-10-19 18:34:13 +0200

    add simple_color namespace and class

--- include/simple_color/color.h
+++ include/simple_color/color.h
@@ -5,13 +5,43 @@
 #ifndef SIMPLE_COLOR_COLOR_H
 #define SIMPLE_COLOR_COLOR_H
 
-#include <iostream>
-#include <mutex>
+#include <string>
 
 namespace simple_color {
 
+    static const char Normal[] = {0x1b, '[', '0', ';', '3', '9', 'm', 0};
+    static const char Red[] = {0x1b, '[', '1', ';', '3', '1', 'm', 0};
+    static const char Green[] = {0x1b, '[', '1', ';', '3', '2', 'm', 0};
+    static const char Yellow[] = {0x1b, '[', '1', ';', '3', '3', 'm', 0};
+    static const char Blue[] = {0x1b, '[', '1', ';', '3', '4', 'm', 0};
+    static const char Magenta[] = {0x1b, '[', '1', ';', '3', '5', 'm', 0};
+    static const char Cyan[] = {0x1b, '[', '1', ';', '3', '6', 'm', 0};
+    static const char White[] = {0x1b, '[', '1', ';', '3', '7', 'm', 0};
+    static const char BRed[] = {0x1b, '[', '1', ';', '9', '1', 'm', 0};
+    static const char BGreen[] = {0x1b, '[', '1', ';', '9', '2', 'm', 0};
+    static const char BYellow[] = {0x1b, '[', '1', ';', '9', '3', 'm', 0};
+    static const char BBlue[] = {0x1b, '[', '1', ';', '9', '4', 'm', 0};
+    static const char BMagenta[] = {0x1b, '[', '1', ';', '9', '5', 'm', 0};
+    static const char BCyan[] = {0x1b, '[', '1', ';', '9', '6', 'm', 0};
+    static const char BWhite[] = {0x1b, '[', '1', ';', '9', '7', 'm', 0};
 
-    
+    static const std::string sNormal = std::string(Normal);
+    static const std::string sRed = std::string(Red);
+    static const std::string sGreen = std::string(Green);
+    static const std::string sYellow = std::string(Yellow);
+    static const std::string sBlue = std::string(Blue);
+    static const std::string sMagenta = std::string(Magenta);
+    static const std::string sCyan = std::string(Cyan);
+    static const std::string sWhite = std::string(White);
+    static const std::string sBRed = std::string(BRed);
+    static const std::string sBGreen = std::string(BGreen);
+    static const std::string sBYellow = std::string(BYellow);
+    static const std::string sBBlue = std::string(BBlue);
+    static const std::string sBMagenta = std::string(BMagenta);
+    static const std::string sBCyan = std::string(BCyan);
+    static const std::string sBWhite = std::string(BWhite);
+
+    std::string give_color(const std::string &color, const std::string &str);
 }
 
 #endif //SIMPLE_COLOR_COLOR_H
--- src/color.cpp
+++ src/color.cpp
@@ -5,5 +5,37 @@
 #include "simple_color/color.h"
 
 namespace simple_color {
-
+    std::string give_color( const std::string &color, const std::string &str) {
+        if (color == "red") {
+            return sRed + str + sNormal;
+        } else if (color == "green") {
+            return sGreen + str + sNormal;
+        } else if (color == "yellow") {
+            return sYellow + str + sNormal;
+        } else if (color == "blue") {
+            return sBlue + str + sNormal;
+        } else if (color == "magenta") {
+            return sMagenta + str + sNormal;
+        } else if (color == "cyan") {
+            return sCyan + str + sNormal;
+        } else if (color == "white") {
+            return sWhite + str + sNormal;
+        } else if (color == "bred") {
+            return sBRed + str + sNormal;
+        } else if (color == "bgreen") {
+            return sBGreen + str + sNormal;
+        } else if (color == "byellow") {
+            return sBYellow + str + sNormal;
+        } else if (color == "bblue") {
+            return sBBlue + str + sNormal;
+        } else if (color == "bmagenta") {
+            return sBMagenta + str + sNormal;
+        } else if (color == "bcyan") {
+            return sBCyan + str + sNormal;
+        } else if (color == "bwhite") {
+            return sBWhite + str + sNormal;
+        } else {
+            return str;
+        }
+    }
 }

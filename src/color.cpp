92a42a9afa5a2f548258fda95474da3c9b521d76
Joaquin Bejar <jbejar@swissblock.net>
2023-10-19 18:39:23 +0200

    add simple_color namespace and class

--- src/color.cpp
+++ src/color.cpp
@@ -5,37 +5,31 @@
 #include "simple_color/color.h"
 
 namespace simple_color {
-    std::string give_color( const std::string &color, const std::string &str) {
-        if (color == "red") {
-            return sRed + str + sNormal;
-        } else if (color == "green") {
-            return sGreen + str + sNormal;
-        } else if (color == "yellow") {
-            return sYellow + str + sNormal;
-        } else if (color == "blue") {
-            return sBlue + str + sNormal;
-        } else if (color == "magenta") {
-            return sMagenta + str + sNormal;
-        } else if (color == "cyan") {
-            return sCyan + str + sNormal;
-        } else if (color == "white") {
-            return sWhite + str + sNormal;
-        } else if (color == "bred") {
-            return sBRed + str + sNormal;
-        } else if (color == "bgreen") {
-            return sBGreen + str + sNormal;
-        } else if (color == "byellow") {
-            return sBYellow + str + sNormal;
-        } else if (color == "bblue") {
-            return sBBlue + str + sNormal;
-        } else if (color == "bmagenta") {
-            return sBMagenta + str + sNormal;
-        } else if (color == "bcyan") {
-            return sBCyan + str + sNormal;
-        } else if (color == "bwhite") {
-            return sBWhite + str + sNormal;
+    std::unordered_map<std::string, std::string> color_map = {
+            {"red", sRed},
+            {"green", sGreen},
+            {"yellow", sYellow},
+            {"blue", sBlue},
+            {"magenta", sMagenta},
+            {"cyan", sCyan},
+            {"white", sWhite},
+            {"bred", sBRed},
+            {"bgreen", sBGreen},
+            {"byellow", sBYellow},
+            {"bblue", sBBlue},
+            {"bmagenta", sBMagenta},
+            {"bcyan", sBCyan},
+            {"bwhite", sBWhite},
+    };
+
+    std::string give_color(const std::string &color, const std::string &str) {
+        auto it = color_map.find(color);
+        if (it != color_map.end()) {
+            return it->second + str + sNormal;
         } else {
             return str;
         }
     }
+
+
 }

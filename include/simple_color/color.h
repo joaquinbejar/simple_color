b98f9442feb324483e8a0fe774e38c72ebd0d8e7
Joaquin Bejar <jbejar@swissblock.net>
2023-10-19 18:25:00 +0200

    first commit

---
+++ include/simple_color/color.h
@@ -0,0 +1,17 @@
+//
+// Created by Joaquin Bejar Garcia on 19/10/23.
+//
+
+#ifndef SIMPLE_COLOR_COLOR_H
+#define SIMPLE_COLOR_COLOR_H
+
+#include <iostream>
+#include <mutex>
+
+namespace simple_color {
+
+
+    
+}
+
+#endif //SIMPLE_COLOR_COLOR_H
---
+++ src/color.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Joaquin Bejar Garcia on 19/10/23.
+//
+
+#include "simple_color/color.h"
+
+namespace simple_color {
+
+}

b087994095b82cedf252e0f26b10d3ff03c7a872
Joaquin Bejar <jbejar@swissblock.net>
2023-10-23 18:30:53 +0200

    update how works give_color

--- test/test_simple_color.cpp
+++ test/test_simple_color.cpp
@@ -1,3 +1,22 @@
 //
 // Created by Joaquin Bejar Garcia on 19/10/23.
 //
+
+#include <catch2/catch_test_macros.hpp>
+#include <simple_color/color.h>
+#include <iostream>
+
+
+
+using namespace simple_color;
+
+
+TEST_CASE("Test give_color function", "[give_color]") {
+
+    SECTION("Orange and bright orange should work") {
+
+        std::cout << give_color(Colors::DARKORANGE, "DARKORANGE") << std::endl;
+        std::cout << give_color(Colors::ORANGE1, "ORANGE1") << std::endl;
+        std::cout << give_color(Colors::ORANGE2, "ORANGE2") << std::endl;
+    }
+}
